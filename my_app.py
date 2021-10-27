794f8cfa752e3f20d8946c94c43d1a6db3fca4fa
erin dyal <erindyal@erins-iMac.local>
2021-10-27 09:29:19 -0400

    wed am

---
+++ my_app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import plotly.express as px
+import numpy as np
+import pickle
+# from predict import predict_hotdog
+
+st.title("Our Hotdog Classifier App")
+st.header("It classifies an image as a hotdog or not a hotdog.")
+st.subheader("Cool, huh?")
