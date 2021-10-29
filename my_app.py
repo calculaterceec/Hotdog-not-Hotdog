a4925dff6b8eb33fdd647bc6edfec729d5421f5b
erin dyal <erindyal@erins-iMac.local>
2021-10-29 09:45:05 -0400

    fri am

--- my_app.py
+++ my_app.py
@@ -7,3 +7,4 @@ from predict import predict_hotdog
 st.title("Our Hotdog Classifier App")
 st.header("It classifies an image as a hotdog or not a hotdog.")
 st.subheader("Cool, huh? Let's try to change something to see if it goes through.")
+st.header("testing")
